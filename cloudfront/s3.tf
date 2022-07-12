441c12a7db00f18389dbceb57b3068e2b0defc90
will Farrell <will.farrell@gmail.com>
2022-07-12 13:17:16 -0600

    fix: refactor s3 config

--- cloudfront/s3.tf
+++ cloudfront/s3.tf
@@ -1,9 +1,28 @@
 resource "aws_s3_bucket" "main" {
   count = var.bucket_domain_name == "" ? 1 : 0
   bucket              = "${local.name}-${terraform.workspace}-static-assets"
-  acl                 = "private"
-  acceleration_status = "Enabled"
 
+  tags = merge(
+    local.tags,
+    {
+      Name     = "${local.name} Static Assets"
+      Security = "SSE:AWS"
+    }
+  )
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_accelerate_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+  status = "Enabled"
+}
+
+resource "aws_s3_bucket_cors_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
@@ -15,40 +34,38 @@ resource "aws_s3_bucket" "main" {
     ]
     max_age_seconds = 3000
   }
-
-  #versioning {
-  #  enabled = false
-  #}
-
-  logging {
-    target_bucket = local.logging_bucket
-    target_prefix = "AWSLogs/${local.account_id}/S3/${local.name}-${terraform.workspace}-static-assets/"
-  }
-
-  // Requires origin-request (https://aws.amazon.com/blogs/networking-and-content-delivery/serving-sse-kms-encrypted-content-from-s3-using-cloudfront/)
-  //  server_side_encryption_configuration {
-  //    rule {
-  //      apply_server_side_encryption_by_default {
-  //        sse_algorithm = "aws:kms"
-  //      }
-  //    }
-  //  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  tags = merge(
-    local.tags,
-    {
-      Name     = "${local.name} Static Assets"
-      Security = "SSE:AWS"
-    }
-  )
 }
+
+resource "aws_s3_bucket_logging resource" "main" {
+  bucket = aws_s3_bucket.main.id
+  target_bucket = local.logging_bucket
+  target_prefix = "AWSLogs/${local.account_id}/S3/${local.name}-${terraform.workspace}-static-assets/"
+}
+
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+// Requires origin-request (https://aws.amazon.com/blogs/networking-and-content-delivery/serving-sse-kms-encrypted-content-from-s3-using-cloudfront/)
+//  server_side_encryption_configuration {
+//    rule {
+//      apply_server_side_encryption_by_default {
+//        sse_algorithm = "aws:kms"
+//      }
+//    }
+//  }
+
 
 resource "aws_s3_bucket_public_access_block" "main" {
   count = var.bucket_domain_name == "" ? 1 : 0
